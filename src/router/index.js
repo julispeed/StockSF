3902bc94bb866d88413444926642496724da9a60
julispeed <julian.acuna2003@gmail.com>
2025-08-25 13:39:55 -0300

    Listado Stock Simple

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Articulos from '@/views/Articulos.vue'
 import Movimientos from '@/views/Movimientos.vue'
-import CreateDeposito from '@/views/Depositos.vue'
-import CreateProveedor from '@/views/Proveedor.vue'
-
+import Deposito from '@/views/Depositos.vue'
+import Proveedor from '@/views/Proveedor.vue'
+import Listados from '@/views/Listados.vue'
 import Inicio from '@/views/Inicio.vue' 
 
 
 const routes = [
   { path: '/articulos', name: 'Articulos', component: Articulos },
-  { path: '/movimientos', name: 'Egreso', component: Movimientos },
-  { path: '/Depositos', name: 'CreateDeposito', component: CreateDeposito },
-  { path: '/Proveedor', name: 'CreateProveedor', component: CreateProveedor}, 
+  { path: '/movimientos', name: 'Movimientos', component: Movimientos },
+  { path: '/Depositos', name: 'Depositos', component: Deposito },
+  { path: '/Proveedor', name: 'Proveedores', component: Proveedor}, 
+  { path: '/Listados', name: 'Listados', component: Listados}, 
 ]
 
 
